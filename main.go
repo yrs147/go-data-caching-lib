ce416c334a40003af93f915cd8bd53fb413f9e0a
Yash Raj <yashraj14700728@gmail.com>
2023-04-18 22:54:27 +0530

    Fist commit

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+type User struct {
+	ID       int
+	Username string
+}
+
+type Server struct {
+	db map[int]*User
+}
+
+func NewServer() *Server {
+	db := make(map[int]*User)
+
+	for i := 1; i < 100; i++ {
+		db[i+1] = &User{
+			ID:       i + 1,
+			Username: fmt.Sprintf("iser_%d", i+1),
+		}
+	}
+	return &Server{
+		db: db,
+	}
+}
+
+func (s *Server) handleGetUser(w http.ResponseWriter, r *http.Request) {
+	idStr := r.URL.Query().Get("id")
+	id, _ := strconv.Atoi(idStr)
+
+	user, ok := s.db[id]
+	if !ok {
+		panic("user not found")
+	}
+
+	json.NewEncoder(w).Encode(user)
+}
+
+func main() {
+
+}
