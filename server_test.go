bd6e475f44df575ae401d12b89b05c0584f14357
Yash Raj <yashraj14700728@gmail.com>
2023-04-18 23:19:26 +0530

    Added dbhit

--- server_test.go
+++ server_test.go
@@ -1,24 +1,31 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 )
 
 func TestHandleGetUser(t *testing.T) {
 	s := NewServer()
-	ts := NewServer(http.HandlerFunc(s.handleGetUser))
+	ts := httptest.NewServer(http.HandlerFunc(s.handleGetUser))
 	nreq := 1000
 
-	for i:= 0;i<nreq;i++ {
-		id := i %100 +1
-		url := fmt.Sprintf("%s/?id=%d",ts.URL , id)
-		resp,err := http.Get(url)
-		if err !=nil {
+	for i := 0; i < nreq; i++ {
+		id := i%100 + 1
+		url := fmt.Sprintf("%s/?id=%d", ts.URL, id)
+		resp, err := http.Get(url)
+		if err != nil {
 			t.Error(err)
 		}
 
 		user := &User{}
-		if err := json.NewDecoder()
+		if err := json.NewDecoder(resp.Body).Decode(user); err != nil {
+			t.Error(err)
+		}
+		fmt.Printf("%+v\n", user)
+		fmt.Println("times we have hit the database: ", s.dbhit)
 	}
 }
