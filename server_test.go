9d38bf340714326ee50e9a8371b13660e0c0543c
Yash Raj <yashraj14700728@gmail.com>
2023-04-18 23:00:55 +0530

    Added 1000 req simulating function

--- server_test.go
+++ server_test.go
@@ -1,5 +1,24 @@
 package main
 
-func TestHandleGetUser (t *testing.T) {
-	s := NewServer(http.HandlerFunc())
+import (
+	"net/http"
+	"testing"
+)
+
+func TestHandleGetUser(t *testing.T) {
+	s := NewServer()
+	ts := NewServer(http.HandlerFunc(s.handleGetUser))
+	nreq := 1000
+
+	for i:= 0;i<nreq;i++ {
+		id := i %100 +1
+		url := fmt.Sprintf("%s/?id=%d",ts.URL , id)
+		resp,err := http.Get(url)
+		if err !=nil {
+			t.Error(err)
+		}
+
+		user := &User{}
+		if err := json.NewDecoder()
+	}
 }
